29f22fd51a85affec9d9422356dd784428f43d23
psalv <psalvato@uwo..ca>
2016-12-29 15:59:52 -0500

    Fixed spacing issues with navbar.

--- public_html/html_elements/triangleGallery.php
+++ public_html/html_elements/triangleGallery.php
@@ -5,7 +5,7 @@ This module relies on code in main.js in order to properly function with resizin
 
 -->
 
-<nav class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-fixed-top">
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1000" height="200" id="svg1">
         <defs>
 
@@ -52,5 +52,6 @@ This module relies on code in main.js in order to properly function with resizin
         <image xlink:href="//placehold.it/504x504/5bc0de" clip-path="url(#clip6)" id="im5"></image>
 
     </svg>
-
 </nav>
+
+<div id="spacer"></div>
