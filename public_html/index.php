64ea270a6ccaec2ac9a140fd7724277c33152bac
Michael Song <songhaozhou@gmail.com>
2016-12-30 16:46:26 -0500

    Added Executives to index

--- public_html/index.php
+++ public_html/index.php
@@ -99,8 +99,59 @@
     <!-- End of Client Application -->
 
     <!-- Beginning of Exec -->
-    <div>
-
+    <div class="container">
+        <!-- Introduction Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Our Team</h1>
+            </div>
+        </div>
+        <hr>
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/HarrisonC.jpg" alt="">
+                <h3>Harrison Chow
+                    <br><small>President</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/MichaelP.jpg" alt="">
+                <h3>Michael Park
+                    <br><small>VP Tech</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/ChristinaC.jpg" alt="">
+                <h3>Christina Chan
+                    <br><small>VP Design</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/AnwarJ.jpg" alt="">
+                <h3>Anwar Jeffrey
+                    <br><small>VP Finance & Legal</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/Placeholder.jpg" alt="">
+                <h3>Eun Su Shum
+                    <br><small>Techincal Product Manager</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/Placeholder.jpg" alt="">
+                <h3>Paul Salvatore
+                    <br><small>Developer</small>
+                </h3>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-responsive" src="./assets/img/team/Placeholder.jpg" alt="">
+                <h3>Alex Madrzyk
+                    <br><small>Developer</small>
+                </h3>
+            </div>
+        </div>
     </div>
     <!-- End of Exec -->
     <!-- End of Contents -->
