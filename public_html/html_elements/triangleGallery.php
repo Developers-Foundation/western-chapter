ab34022a6a53d480e21ddc92e745bffce3006c5d
psalv <psalvato@uwo..ca>
2016-12-31 13:30:04 -0500

    Added transition from lander to solid nav bar.

--- public_html/html_elements/triangleGallery.php
+++ public_html/html_elements/triangleGallery.php
@@ -7,12 +7,14 @@ This module relies on code in main.js in order to properly function with resizin
 
 <nav class="navbar navbar-default navbar-fixed-top" id="triangleNav">
 
+    <!--
     <row id="triLabels" style="position: fixed; width: 100%">
         <div class="col-xs-2 triLbl" id="triLbl1">Developers' Foundation</div>
         <div class="col-xs-4 triLbl text-center" id="triLbl2">Our Work</div>
         <div class="col-xs-4 triLbl text-center" id="triLbl3">Join Us</div>
         <div class="col-xs-2 triLbl text-right" id="triLbl4">Misc Title</div>
     </row>
+    -->
 
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1000" height="200" id="svg1">
         <defs>
@@ -21,43 +23,51 @@ This module relies on code in main.js in order to properly function with resizin
 
             <!-- Equilateral Triangle -->
             <clipPath id="clip1">
-                <polygon points="50,0 0,100 100,100" id="poly1">  <!-- Coordinate system is x,y with 0,0 being the top left corner -->
+                <polygon points="50,0 0,100 100,100" class="poly1">  <!-- Coordinate system is x,y with 0,0 being the top left corner -->
             </clipPath>
 
             <!-- Upside down Equilateral Triangle -->
             <clipPath id="clip2">
-                <polygon points="0,0 100,0 50,100" id="poly2">
+                <polygon points="0,0 100,0 50,100" class="poly2" fill="black">
             </clipPath>
-
 
             <!-- Right Angle Triangle (Right) (unused) -->
             <clipPath id="clip3">
-                <polygon points="50,0 50,100 0,100" id="poly3">
+                <polygon points="50,0 50,100 0,100" class="poly3">
             </clipPath>
             <!-- Right Angle Triangle (Left) (unused) -->
             <clipPath id="clip4">
-                <polygon points="50,0 50,100 100,100" id="poly4">
+                <polygon points="50,0 50,100 100,100" class="poly4">
             </clipPath>
 
             <!-- Square Side Triangle (Left) -->
             <clipPath id="clip5">
-                <polygon points="0,0 50,0 100,100 0,100" id="poly5">
+                <polygon points="0,0 50,0 100,100 0,100" class="poly5">
             </clipPath>
 
             <!-- Square Side Triangle (Right) -->
             <clipPath id="clip6">
-                <polygon points="100,0 100,100 0,100 50,0" id="poly6">
+                <polygon points="100,0 100,100 0,100 50,0" class="poly6">
             </clipPath>
 
         </defs>
 
         <!-- We create each image and assign the required variables.
         Important variable to note are the clip-path (to create the triangle) and translate (so they aren't all overlapping) -->
-        <image xlink:href="//placehold.it/500x500/800080" clip-path="url(#clip5)" id="im1"></image>
-        <image xlink:href="//placehold.it/501x501/800080" clip-path="url(#clip2)" id="im2"></image>
-        <image xlink:href="//placehold.it/502x502/800080" clip-path="url(#clip1)" id="im3"></image>
-        <image xlink:href="//placehold.it/503x503/800080" clip-path="url(#clip2)" id="im4"></image>
-        <image xlink:href="//placehold.it/504x504/800080" clip-path="url(#clip6)" id="im5"></image>
+        <image xlink:href="//placehold.it/500x500/800080" clip-path="url(#clip5)" class="im1 "></image>
+        <polygon class="poly5 im1 scrolledNav" fill="#EE82EE"></polygon>
+
+        <image xlink:href="//placehold.it/501x501/800080" clip-path="url(#clip2)" class="im2"></image>
+        <polygon class="poly2 im2 scrolledNav" fill="#EE82EE"></polygon>
+
+        <image xlink:href="//placehold.it/502x502/800080" clip-path="url(#clip1)" class="im3"></image>
+        <polygon class="poly1 im3 scrolledNav" fill="#EE82EE"></polygon>
+
+        <image xlink:href="//placehold.it/503x503/800080" clip-path="url(#clip2)" class="im4"></image>
+        <polygon class="poly2 im4 scrolledNav" fill="#EE82EE"></polygon>
+
+        <image xlink:href="//placehold.it/504x504/800080" clip-path="url(#clip6)" class="im5"></image>
+        <polygon class="poly6 im5 scrolledNav" fill="#EE82EE"></polygon>
 
     </svg>
 </nav>
