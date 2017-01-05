dfc22d6332a7dec2be5e793a24e83e144d270264
Michael Park <michael511.mp@gmail.com>
2017-01-04 19:23:02 -0500

    more edit on our story

--- public_html/index.php
+++ public_html/index.php
@@ -73,15 +73,14 @@
                 <h1>Our Story</h1>
             </div>
             <div class="col-md-4">
-                <i class="fa fa-user fa-2x" aria-hidden="true"></i>
-                <p class="description-header">Current Efforts</p>
+                <p class="description-header"><i class="fa fa-user" aria-hidden="true"></i>Current Efforts</p>
                 <p class="description">As of January 2017, Developers' Foundation has successfully expanded into three
                     campuses, at Western University, Queen's and UNICAMP (Brazil). Having successfully
                     chartered Western's chapter, Developers' Foundation has developed over a dozen
                     functional websites for Student and Community organizations.</p>
             </div>
             <div class="col-md-4">
-                <p class="description-header">Future Efforts</p>
+                <p class="description-header"> <i class="fa fa-code" aria-hidden="true"></i> Future Efforts</p>
                 <p class="description">Throughout the first semester your clients will concentrate solely on catering to Student
                     organizations and charities/NPO. During the second semester of this school year, HQ will
                     recommend to include one small business client per month. This will be DF’s main source
@@ -90,7 +89,7 @@
                     of chapters, from 3 to 6 by the end of the second semester.</p>
             </div>
             <div class="col-md-4">
-                <p class="description-header">Content Management System (CMS)</p>
+                <p class="description-header"> <i class="fa fa-archive" aria-hidden="true"></i> Content Management System (CMS)</p>
                 <p class="description">We have also built our very own CMS (think wordpress), to make it easier for both our clients
                     and us when it comes to content maintenance. We believe this has a huge value in also
                     building an ecosystem where NPO and charities can rely on.</p>
