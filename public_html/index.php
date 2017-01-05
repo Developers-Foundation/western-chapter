0dc4b9712058157aa029225c036046df06100115
Nobody Random <iamnobodyrandom@yahoo.com>
2017-01-04 20:47:31 -0500

    typo

--- public_html/index.php
+++ public_html/index.php
@@ -203,7 +203,7 @@
             </div>
             <div class="col-lg-4 col-sm-6 text-center">
                 <img class="img-responsive" src="./assets/img/team/Placeholder.jpg" alt="">
-                <h3>Eun Su Shum
+                <h3>Eun Su Shim
                     <br><small>Techincal Product Manager</small>
                 </h3>
             </div>
