0ab2cd6f4a1d9565646c344cf35169d7b32fd2b7
psalv <psalvato@uwo..ca>
2017-01-09 22:08:40 -0500

    Changed our story, mission statement, and portfolio to be responsive.

---
+++ public_html/html_elements/missionStatement.php
@@ -0,0 +1,18 @@
+<!-- Beginning of Mission Statement -->
+<div class="row" id="mission-statement">
+
+    <div class="col-sm-6 text-center">
+        <h1>Mission Statement: <br>Build Something <span class="purple">meaningful</span></h1>
+    </div>
+
+    <div class="col-sm-6">
+        <p>
+            We want to help underserved communities to become sustainably independent, through the leverage of
+            technology.
+            <br><br>
+            We are group of students and aspiring designers/developers from The University of Western Ontario
+            which serves as a subteam for Developers' Foundation.
+        </p>
+    </div>
+
+</div>
---
+++ public_html/html_elements/ourStory.php
@@ -0,0 +1,27 @@
+<div class="row" id="our-story">
+    <div class="col-md-12">
+        <h1>Our Story</h1>
+    </div>
+    <div class="col-md-4 descr">
+        <p class="description-header"><i class="fa fa-user" aria-hidden="true"></i> Current Efforts</p>
+        <p class="description">As of January 2017, Developers' Foundation has successfully expanded into three
+            campuses, at Western University, Queen's and UNICAMP (Brazil). Having successfully
+            chartered Western's chapter, Developers' Foundation has developed over a dozen
+            functional websites for Student and Community organizations.</p>
+    </div>
+    <div class="col-md-4 descr">
+        <p class="description-header"> <i class="fa fa-code" aria-hidden="true"></i> Future Efforts</p>
+        <p class="description">Throughout the first semester your clients will concentrate solely on catering to Student
+            organizations and charities/NPO. During the second semester of this school year, HQ will
+            recommend to include one small business client per month. This will be DF’s main source
+            of financial support. On average, we expected each chapter to work on at least 3 clients per
+            month. Another HQ initiative that we super excited about, is to increase the current number
+            of chapters, from 3 to 6 by the end of the second semester.</p>
+    </div>
+    <div class="col-md-4 descr">
+        <p class="description-header"> <i class="fa fa-archive" aria-hidden="true"></i> Content Management System (CMS)</p>
+        <p class="description">We have also built our very own CMS (think wordpress), to make it easier for both our clients
+            and us when it comes to content maintenance. We believe this has a huge value in also
+            building an ecosystem where NPO and charities can rely on.</p>
+    </div>
+</div>
