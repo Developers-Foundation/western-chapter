8ae5a0978917f934a0ff94c97758855da869ab98
psalv <psalvato@uwo.ca>
2017-01-25 22:26:10 -0500

    A lot of changes include: -Added up nav button -centered mission statement on scroll -condensed our story -grayscale portfolio until hover -added triangle background to application -reduced footer size
    Still need to deal with scroll positioning (to make things full screen) as well as placement of various divs.

--- public_html/html_elements/ourStory.php
+++ public_html/html_elements/ourStory.php
@@ -4,27 +4,31 @@
     </div>
     <div class="col-md-4 descr">
         <p class="description-header"><br><i class="fa fa-user" aria-hidden="true"></i> Current Efforts</p>
-        <p class="description">As of January 2017, Developers' Foundation has successfully expanded into three
-            campuses, at Western University, Queen's and UNICAMP (Brazil). Having successfully
-            chartered Western's chapter, Developers' Foundation has developed over a dozen
-            functional websites for Student and Community organizations.</p>
+        <div class="col-md-12 descr">
+            <p class="description">
+                As of January 2017, Developers' Foundation has successfully expanded into three
+                campuses, at Western University, Queen's and UNICAMP (Brazil). Having successfully
+                chartered Western's chapter, Developers' Foundation has developed over a dozen
+                functional websites for Student and Community organizations.
+            </p>
+        </div>
     </div>
-    <div class="col-md-4 descr">
-        <p class="description-header"><br><i class="fa fa-eye" aria-hidden="true"></i> The Developers' Foundation Vision</p>
-        <p class="description">To become a global changemaker in society through the creativity, energy and passion of students.<br>
-            <br><b>Our Bottom Line:</b><br>
-            We build technologies that create change, while providing students industry experience with organizations already making a difference.
-        </p>
-    </div>
-    <div class="col-md-4 descr">
+
+    <div class="col-md-8 descr">
         <p class="description-header"><br><i class="fa fa-check-square" aria-hidden="true"></i> Our Objectives</p>
-        <p class="description">
-            <ul class="description">
-            <li>Support and empower organizations with technology solutions to better tackle their causes.</li>
-            <li>Expose students to humanitarian engineering by working with organizations that need technology solutions.</li>
-            <li>Educate students to gain an increased awareness of poverty and their individual responsibility as Global Citizens.</li>
-            <li>Educate students to graduate with industry skills through their work experience at Developers' Foundation with organization</li>
-        </ul>
-        </p>
+        <div class="col-md-6 descr">
+            <p class="description">
+                Support and empower organizations with technology solutions to better tackle their causes.<br>
+                Expose students to humanitarian engineering by working with organizations that need technology solutions.
+            </p>
+        </div>
+        <div class="col-md-6 descr">
+            <p class="description">
+                Educate students to gain an increased awareness of poverty and their individual responsibility as Global Citizens.<br>
+                Educate students to graduate with industry skills through their work experience at Developers' Foundation with organization
+            </p>
+        </div>
     </div>
+
+
 </div>
