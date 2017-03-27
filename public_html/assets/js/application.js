ef65b1e262407bc5faee203550a8236592db9220
Nobody Random <iamnobodyrandom@yahoo.com>
2017-03-27 19:36:23 -0400

    buttom size

--- public_html/assets/js/application.js
+++ public_html/assets/js/application.js
@@ -114,7 +114,7 @@ function formSubmit(theForm) {
     }).then(function(obj) {
         console.log("success");
         console.log(theForm);
-        document.getElementById("theForm").parentNode.innerHTML = "<div style='text-align: left;margin-top: 100px'><h3>Thank You for Submitting your Application.</h3><p>We will get back to you as soon as possible <span id='lol'>_</span></p></div>";
+        document.getElementById("theForm").parentNode.innerHTML = "<div style='text-align: left;margin-top: 100px'><h3>Thank You for Submitting your Application</h3><p>We will get back to you as soon as possible <span id='lol'>_</span></p></div>";
     }, function(err) {
         console.log("error");
         console.log(err);
