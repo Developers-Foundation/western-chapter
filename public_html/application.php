283a1fd408f44024a9457c8d30f80adaa289cbe7
Nobody Random <iamnobodyrandom@yahoo.com>
2017-03-27 20:35:34 -0400

    open graph stuff

--- public_html/application.php
+++ public_html/application.php
@@ -7,8 +7,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Join Developers' Foundation Western Chapter Now">
+    <meta name="author" content="Developers' Foundation">
     <title>Developers' Foundation</title>
     <!-- Open Graph
     ================================================== -->
